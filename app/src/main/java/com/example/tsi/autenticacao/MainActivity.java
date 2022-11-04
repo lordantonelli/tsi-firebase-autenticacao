e58ae6d408e4eaad640c431b45e0bc26f038a1ab
Humberto Lidio Antonelli <humbertoantonelli@usp.br>
2022-11-04 08:09:36 -0400

    Projeto inicial

--- app/src/main/java/com/example/tsi/autenticacao/MainActivity.java
+++ app/src/main/java/com/example/tsi/autenticacao/MainActivity.java
@@ -25,8 +25,6 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSair;
     private TextView lblEstado;
     private Button btnNovo;
-    // Atributo que é referência para o gerenciador de usuários:
-    private FirebaseAuth usuarios = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +44,7 @@ public class MainActivity extends AppCompatActivity {
         // Cria e associa o escutador do botão SAIR:
         btnSair.setOnClickListener(new EscutadorBotaoSair());
 
-        // O usuário logado "fica" entre execuções do app no celular.
-        // Então, logo de cara vamos ver se tem alguém logado.
-        // Se tiver, indicamos isso em lblEstado:
-        if (usuarios.getCurrentUser() != null) {
-            lblEstado.setText(
-                "Tem um usuário logado no sistema: "
-                + usuarios.getCurrentUser().getEmail()
-            );
-        }
+
     }
 
 
@@ -64,39 +54,8 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             // Aviso de início de processo:
             lblEstado.setText("Tentando criar novo usuário...");
-            // Verifica se o usuário já está logado:
-            if (usuarios.getCurrentUser() != null) {
-                // Exibe mensagem de usuário já logado, em lblEstado:
-                lblEstado.setText("Já tem um usuário logado!!" + usuarios.getCurrentUser().getEmail());
-            } else {
-                // Pega email e senha na interface:
-                String email = txtEmail.getText().toString();
-                String senha = txtSenha.getText().toString();
-                // Tenta criar o usuário:
-                // ATENCAO!!!
-                // O teste se foi sucesso ou não é ASSINCRONO,
-                // isto é, pode levar um tempo para o resultado voltar do Firebase.
-                // Não pense neste processo como uma programação tradicional!!!
-                usuarios.createUserWithEmailAndPassword(email, senha)
-                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            // Testa se criou o usuário com sucesso:
-                            if (task.isSuccessful()) {
-                                // Criou e logou com sucesso.
-                                // Exibe mensagem em lblEstado:
-                                lblEstado.setText("Usuário criado e logado com sucesso!" + usuarios.getCurrentUser().getEmail());
-                            } else {
-                                // Não conseguiu criar o usuário.
-                                // Exibe mensagem em lblEstado:
-                                lblEstado.setText("Criação do usuário falhou!");
-                                // Exibe a mensagem de erro do Firebase num Toast:
-                                FirebaseAuthException e = (FirebaseAuthException)task.getException();
-                                Toast.makeText(MainActivity.this, "ERRO: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-            }
+
+
         }
     }
 
@@ -106,33 +65,8 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             // Aviso de início de processo:
             lblEstado.setText("Tentando logar no Firebase...");
-            // Verifica se o usuário já está logado:
-            if (usuarios.getCurrentUser() != null) {
-                // Exibe mensagem de usuário já logado, em lblEstado:
-                lblEstado.setText("Já tem um usuário logado!!" + usuarios.getCurrentUser().getEmail());
-            } else {
-                // Pega email e senha na interface:
-                String email = txtEmail.getText().toString();
-                String senha = txtSenha.getText().toString();
-                // Tenta logar:
-                usuarios.signInWithEmailAndPassword(email, senha)
-                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            // Testa se logou com sucesso:
-                            if (task.isSuccessful()) {
-                                // Exibe mensagem em lblEstado:
-                                lblEstado.setText("Usuário logado com sucesso!" + usuarios.getCurrentUser().getEmail());
-                            } else {
-                                // Exibe mensagem em lblEstado:
-                                lblEstado.setText("Login falhou!");
-                                // Exibe a mensagem de erro do Firebase num Toast:
-                                FirebaseAuthException e = (FirebaseAuthException)task.getException();
-                                Toast.makeText(MainActivity.this, "ERRO: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-            }
+
+
         }
     }
 
@@ -142,16 +76,8 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             // Aviso de início de processo:
             lblEstado.setText("Tentando deslogar no Firebase...");
-            // Verifica se existe um usuário logado:
-            if (usuarios.getCurrentUser() == null) {
-                // Exibe mensagem que não tem usuário logado, em lblEstado:
-                lblEstado.setText("Não tem um usuário logado!!");
-            } else {
-                // Deslogando...
-                usuarios.signOut();
-                // Exibe mensagem de usuário deslogado:
-                lblEstado.setText("Usuário deslogado!!");
-            }
+
+
         }
     }
 }
